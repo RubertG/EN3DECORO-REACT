8e38ecb5094c9c2f882f5cf34489b2bc25938c0b
Rubert_G <rubertgonzalez5@gmail.com>
2023-03-04 15:57:32 -0500

    Import data

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import './style/contacto.css'
 import './style/section-productos.css'
 import { RooterPrincipal } from './rooter/RooterPrincipal'
 import { DataContext } from './context/DataContext'
-import { useLoadData } from './hooks/useLoadProduct'
-
+// import { useLoadData } from './hooks/useLoadProduct'
+import data from '../public/json/main.json'
 
 function App() {
 
-   const { data } = useLoadData()
+   // const { data } = useLoadData()
 
    return (
       <DataContext.Provider value={{ data }}>
